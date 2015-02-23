0098d956c933ad2c3d3e42334c7881d0dee471ae
Victor Martinez <studioquetzal@gmail.com>
2015-02-22 22:04:08 -0600

    Listo Modulo 1 donde se pone las naves

--- app/src/main/java/com/example/victor/naval_war/MainActivity.java
+++ app/src/main/java/com/example/victor/naval_war/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.victor.naval_war;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,7 +21,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    boolean[] barcos=new boolean[25];
+    boolean[] barcos=new boolean[23];
     int naves=5;
 
     public void setbarcio(View r){
@@ -60,11 +62,121 @@ public class MainActivity extends ActionBarActivity {
                 btn.setClickable(false);
                 barcos[5]=true;
             }
+            if (r.getId()==R.id.b22){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[6]=true;
+            }
+            if (r.getId()==R.id.b23){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[6]=true;
+            }
+            if (r.getId()==R.id.b24){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[7]=true;
+            }
+            if (r.getId()==R.id.b25){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[8]=true;
+            }
+            if (r.getId()==R.id.b31){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[9]=true;
+            }
+            if (r.getId()==R.id.b32){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[10]=true;
+            }
+            if (r.getId()==R.id.b33){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[11]=true;
+            }
+            if (r.getId()==R.id.b34){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[12]=true;
+            }
+            if (r.getId()==R.id.b35){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[13]=true;
+            }
+            if (r.getId()==R.id.b41){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[14]=true;
+            }
+            if (r.getId()==R.id.b42){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[15]=true;
+            }
+            if (r.getId()==R.id.b43){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[16]=true;
+            }
+            if (r.getId()==R.id.b44){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[17]=true;
+            }
+            if (r.getId()==R.id.b45){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[18]=true;
+            }
+            if (r.getId()==R.id.b51){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[19]=true;
+            }
+            if (r.getId()==R.id.b52){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[20]=true;
+            }
+            if (r.getId()==R.id.b53){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[21]=true;
+            }
+            if (r.getId()==R.id.b54){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[22]=true;
+            }
+            if (r.getId()==R.id.b55){
+                btn.setBackgroundDrawable(getResources().getDrawable(R.drawable.partisannew));
+                btn.setClickable(false);
+                barcos[23]=true;
+            }
 
+        }
+        else {
+            Toast.makeText(getApplicationContext(),"Naves agotadas!!!!!",Toast.LENGTH_SHORT).show();
         }
 
         //ese_we.setBackground(getResources().getDrawable(R.drawable.images_duckduckgo_com));
 
+    }
+
+
+    public void newventana(View r){
+        Intent ventana=new Intent(this,MainActivity2Activity.class);
+        if (naves==0){
+            ventana.putExtra("b",barcos);
+            startActivity(ventana);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Aun te quedan "+naves+" naves!!!!",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
